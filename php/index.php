4a2624ee060eea6da8b709dcc463639df4bd34b8
Nikolaos59 <nikobackland@msn.com>
2022-11-07 14:49:16 +0100

    first commit

---
+++ php/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html Lang="fr">
+        
+    <?php include "./includes/head.html"; ?>
+        
+    <body>
+        <?php
+            include "./includes/header.html";
+            include "./includes/nav.html";
+        ?>
+        <main class="container">
+            <?php
+                if (isset($_GET["about-me"])) {
+                    include "./pages/about-me.html";
+                    return;
+                } elseif (isset($_GET["my-dreams"])) {
+                    include "./pages/my-dreams.html";
+                    return;
+                } elseif (isset($_GET["my-passions"])) {  
+                    include "./pages/my-passions.html";
+                    return;
+                } else {
+                    include "./pages/home.html";
+                    return;
+                }
+            ?>
+       </main>
+
+       <?php include "./includes/footer.html"; ?>
+    </body>
+
+</html>
