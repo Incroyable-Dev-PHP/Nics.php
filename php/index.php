721f7659d1aa26ae2feca6e6be5df29fac86990e
Nikolaos59 <nikobackland@msn.com>
2022-11-08 12:26:00 +0100

    eighth commit

--- php/index.php
+++ php/index.php
@@ -4,30 +4,22 @@
     <?php include "./includes/head.html"; ?>
 </head>
 <body>
-    <header>
-        <?php
-            include "./includes/header.html";?>
-    </header>
-        <main class="container">
+    <?php include "./includes/header.html";?>
+    <?php include "./includes/ul.html"; ?>
+    
+        <section class="container">
             <?php
                 if (isset($_GET["about-me"])) {
                     include "./pages/about-me.html";
-                    return;
                 } elseif (isset($_GET["my-dreams"])) {
                     include "./pages/my-dreams.html";
-                    return;
                 } elseif (isset($_GET["my-passions"])) {  
                     include "./pages/my-passions.html";
-                    return;
                 } else {
                     include "./pages/home.html";
-                    return;
                 }
             ?>
-        </main>
-        <footer>
-            <?php include "./includes/footer.html"; ?>
-        </footer>
-    </body>
-
+        </section>
+    <?php include "./includes/footer.html"; ?>
+</body>
 </html>
